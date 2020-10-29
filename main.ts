babe0f097079b9c79dcf739b938acb3bfe5a0014
MrCottrell <73645309+MrCottrell@users.noreply.github.com>
2020-10-29 11:40:18 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 function Amber () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
     strip.show()
 }
 function Green () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
     strip.show()
 }
 function Red () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     strip.show()
 }
 function RedAmber () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
-    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
     strip.show()
 }
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 strip.clear()
 strip.setBrightness(128)
 basic.forever(function () {
